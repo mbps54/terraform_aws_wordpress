a0e3389e85f5b102d6c2b499c95fbda55038a056
mbps54 <utkin@posteo.net>
2022-06-03 11:53:00 +0300

    STEP-0. Remote state and state locking

---
+++ level_0/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+resource "aws_s3_bucket" "state_locking" {
+  bucket = "state-locking-bucket"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "state-locking-db"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.state_locking.arn
+}
+
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+}
