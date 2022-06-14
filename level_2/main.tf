9e765ed82f4719d830a73343dc09811979b35a62
mbps54 <utkin@posteo.net>
2022-06-14 20:09:11 +0300

    draf using public modules, just a draft

--- level_2/main.tf
+++ level_2/main.tf
@@ -103,7 +103,7 @@ module "sg_ec2" {
   }
 }
 
-module "sg_alb" {
+module "sg_asg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "sg_alb"
@@ -233,18 +233,18 @@ resource "aws_launch_configuration" "launch-config-1" {
   associate_public_ip_address = false
   key_name                    = "my-key-pair-1"
   user_data = templatefile("${path.module}/startup.tpl",
-    { username = var.username,
-      password = var.password,
-  rds_endpoint = var.rds_endpoint })
+    { username     = local.db_creds.username,
+      password     = local.db_creds.password,
+      rds_endpoint = data.db.db_instance_id})
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-#########################################################################
+#######################     EVERYTHING IS BAD BELOW    ########################
 
-module "example_asg" {
+module "asg" {
   source = "terraform-aws-modules/autoscaling/aws"
 
   name = "${local.project}-asg"
@@ -253,56 +253,31 @@ module "example_asg" {
 
   image_id        = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
-  security_groups = ["${data.aws_security_group.default.id}"]
-  load_balancers  = ["${module.elb.this_elb_id}"]
-
-  ebs_block_device = [
-    {
-      device_name           = "/dev/xvdz"
-      volume_type           = "gp2"
-      volume_size           = "50"
-      delete_on_termination = true
-    },
-  ]
-
-  root_block_device = [
-    {
-      volume_size = "50"
-      volume_type = "gp2"
-    },
-  ]
+  security_groups = [module.sg_asg.security_group_id]
+  load_balancers  = [module.elb.elb_id]
 
   # Auto scaling group
-  asg_name                  = "example-asg"
-  vpc_zone_identifier       = ["${data.aws_subnet_ids.all.ids}"]
+  asg_name                  = "${local.project}-asg"
+  vpc_zone_identifier       = local.private_subnets_ids
   health_check_type         = "EC2"
-  min_size                  = 0
-  max_size                  = 1
+  min_size                  = 1
+  max_size                  = 2
   desired_capacity          = 1
   wait_for_capacity_timeout = 0
 
-  tags = [
-    {
-      key                 = "Environment"
-      value               = "dev"
-      propagate_at_launch = true
-    },
-    {
-      key                 = "Project"
-      value               = "megasecret"
-      propagate_at_launch = true
-    },
-  ]
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
-
 
 module "elb" {
   source = "terraform-aws-modules/elb/aws"
 
   name = "${local.project}-elb"
 
-  subnets         = ["${data.aws_subnet_ids.all.ids}"]
-  security_groups = ["${data.aws_security_group.default.id}"]
+  subnets         = local.private_subnets_ids
+  security_groups = [module.sg_asg.security_group_id]
   internal        = false
 
   listener = [
@@ -325,109 +300,7 @@ module "elb" {
   ]
 
   tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-}
-
-#########################################################################
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "${local.project}-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = local.vpc_id
-  subnets            = local.private_subnets_ids
-  security_groups    = [module.sg_alb.security_group_id]
-
-  target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        target-group-1 = {
-          target_id = "i-0123456789abcdefg"
-          port = 80
-        }
-      }
-    }
-  ]
-
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-      target_group_index = 0
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
     Terraform   = "true"
     Environment = "dev"
   }
-}
-
-
-module "asg" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-
-  # Autoscaling group
-  name = "${local.project}-asg"
-
-  min_size                  = 1
-  max_size                  = 2
-  desired_capacity          = 1
-  wait_for_capacity_timeout = 0
-  health_check_type         = "EC2"
-  vpc_zone_identifier       = local.private_subnets_ids
-
-  # Launch template
-  launch_template_name        = "${local.project}-asg-template"
-  launch_template_description = "Launch template"
-  update_default_version      = true
-
-  image_id          = data.aws_ami.ubuntu.id
-  instance_type     = "t2.micro"
-  ebs_optimized     = false
-  enable_monitoring = false
-
-  user_data = 
-
-  tags = {
-    Environment = "dev"
-    Project     = "megasecret"
-  }
-}
-
-module "asg" {
-  source                 = "../modules/asg"
-  project                = local.project
-  subnet_id              = local.private_subnets_ids
-  aws_availability_zones = local.vailability_zones
-  target_group_arns      = module.alb.target_group_arns
-  username               = local.db_creds.username
-  password               = local.db_creds.password
-  rds_endpoint           = module.db.db_instance_endpoint
-  security_group_id      = module.sg_ec2.security_group_id
-}
-
-module "bastion" {
-  source            = "../modules/ec2"
-  project           = local.project
-  subnet_id         = local.public_subnets_ids[0]
-  security_group_id = module.sg_bastion.security_group_id
 }
